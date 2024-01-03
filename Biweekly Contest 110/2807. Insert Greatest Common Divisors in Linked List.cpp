eccbd92dfc4b157caddfff6c5d9dcfb956bdeeb1
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-03 23:01:07 +0530

    1

--- Biweekly Contest 110/2807. Insert Greatest Common Divisors in Linked List.cpp
+++ Biweekly Contest 110/2807. Insert Greatest Common Divisors in Linked List.cpp
@@ -1,15 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode *temp=head;
+        while(temp->next!=NULL)
+        {
+            ListNode *newnode=new ListNode(gcd(temp->val,temp->next->val));
+            newnode->next=temp->next;
+            temp->next=newnode;
+            temp=newnode->next;
+        }
+    return head;
+    }
+};
+
+
 int main(){
  
 
 
 }
 
-
-/*
-15 -> 80
-11 -> 90
-
-*/
