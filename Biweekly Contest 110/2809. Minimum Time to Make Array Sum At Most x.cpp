ee4b8f1e38e144c129fccda87eb304a54321c48d
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-06 17:51:42 +0530

    1

---
+++ Biweekly Contest 110/2809. Minimum Time to Make Array Sum At Most x.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int minimumTime(vector<int>& nums1, vector<int>& nums2, int x) {
+        
+}
+
+
+
+int main() {
+
+  int n;
+  cin >> n;
+
+  vector<int>  arr1(n);
+
+  vector<int>  brr(n);
+
+
+  for (int i = 0; i < n; i++) {
+    cin>>arr1[i];
+  }
+  
+
+  for (int i = 0; i < n; i++) {
+    
+    cin>>brr[i];
+  }
+
+
+  int x;cin>>x;
+
+
+  cout<<minimumTime(arr1,brr,x);
+
+
+  
+}
+
+
+
+
+
+/*
+
+Hi,
+
+I hope this message finds you well! I came across Intern (https://salesforce.wd12.myworkdayjobs.com/en-US/External_Career_Site/job/India---Hyderabad/Intern_JR234461) at Saleforce. As an aspiring Software Engineer, I admire the company's innovative work. If possible, could you kindly refer me? I'd greatly appreciate it!
+
+Best regards,
+Shahbaz Khan
+
+
+
+my resume Link:-
+
+https://drive.google.com/file/d/1h9scmsmnC2gtWvpHtVXhgWWsAqfjNjHz/view?usp=sharing
+
+*/
+
+
--- Biweekly Contest 110/test.cpp
+++ Biweekly Contest 110/test.cpp
@@ -2,62 +2,9 @@
 using namespace std;
 
 
-int lengthOfLIS1(vector<int>& nums) {
-    std::vector<int> tails(nums.size(), 0);
-    int size = 0;
-
-    for (int x : nums) {
-      int i = 0, j = size;
-      while (i != j) {
-                int m = (i + j) / 2;
-                if (tails[m] < x)
-                    i = m + 1;
-                else
-                    j = m;
-      }
-            tails[i] = x;
-            if (i == size) ++size;
-    
+ int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        
     }
-
-
-    return size;
-}
-
-
-
-
-int lengthOfLIS(vector<int> &nums) {
-
-  int n = nums.size();
-
-  vector<int> dp(n, 1);
-
-
-  for (int i = 1; i < n; i++) {
-    for (int j = 0; j < i; j++) {
-      if (nums[j] < nums[i])
-        dp[i] = max(dp[j] + 1, dp[i]);
-      
-    }
-  }
-
-
-
-  // for (auto it : dp)
-  //   cout << it << " ";
-  
-
-  // cout << endl;
-
-  int maxLength = *max_element(dp.begin(), dp.end());
-
-  return maxLength;
-
-
-}
-
-
 
 
 int main()
@@ -74,8 +21,161 @@ int main()
     cin >> arr[i];
   }
 
-  cout << lengthOfLIS1(arr);
+  vector<int>  start(n);
+  vector<int> profit(n);
+
+    for (int i = 0; i < n; i++)
+    {
+      /* code */
+      cin>>start[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+      /* code */
+      cin>>profit[i];
+    }
+
+    cout<<jobScheduling(arr,start,profit)<<endl;
+    
+    
+
+  // cout << j(arr);
 }
 
 
 
+
+
+
+/*
+
+
+
+
+class Solution {
+
+
+
+public:
+    int jobScheduling(vector<int>& s, vector<int>& e, vector<int>& p) {
+       
+        int n = s.size(); 
+
+
+        vector<pair<int, int>> st(n);
+
+
+        for(int i = 0; i < n; i++) st[i] = {s[i], i};
+
+        sort(st.begin(), st.end());
+
+
+        vector<int> dp(n+1, 0); 
+        
+        for(int i = n-1; i > -1; i--)
+            dp[i] += max(dp[i+1], p[st[i].second] + dp[lower_bound(st.begin() + i, st.end(), make_pair(e[st[i].second], 0)) - st.begin()]);
+
+
+        return dp[0];
+
+
+    }
+
+
+
+};
+
+
+
+*/
+
+
+
+/*
+
+
+class Solution {
+public:
+    int numJobs;
+    int memo[50001];
+
+
+    int getNextIndex(vector<vector<int>>&jobs , int l , int end){
+        int r = numJobs - 1;
+        int res = numJobs;
+        while(l <= r){
+            int mid = l + ((r-l) >> 1);
+
+            if(jobs[mid][0] >= end){
+                res = mid;
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return res;
+    }
+    int solve(vector<vector<int>>&jobs ,vector<int>&startTime , int i){
+
+        if(i >= numJobs){
+            return 0;
+        }
+
+
+        //if we have already calculated the max profit of that job
+        if(memo[i] != -1){
+            return memo[i];
+        }
+
+        //include
+        int next = getNextIndex(jobs , i+1 , jobs[i][1]);
+        //we are using binary search here to get the next index
+        //it helps us to find the lower bound of the next safe job to take
+        //so passing jobs , start , end
+
+        //instead we can also use lower_bound function directly but it is always better to write your own function
+
+        // int next = lower_bound(startTime.begin() , startTime.end() , jobs[i][1]) - startTime.begin();
+
+        int inc = jobs[i][2] + solve(jobs , startTime , next);
+
+
+        //not include
+        int not_inc = solve(jobs , startTime, i + 1);
+
+        return memo[i] = max(inc , not_inc);
+    }
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        numJobs = startTime.size();
+        memset(memo , -1 , sizeof(memo));
+        
+        vector<vector<int>>jobs(numJobs , vector<int>(3,0));
+        for(int i = 0 ; i < numJobs ; i++){
+            jobs[i][0] = startTime[i];
+            jobs[i][1] = endTime[i];
+            jobs[i][2] = profit[i];
+        }
+
+        //we can also create jobs in a diff way
+        // vector<tuple<int,int,int>>jobs(numJobs);
+
+        // for(int i = 0 ; i < numJobs ;i++){
+        //     jobs[i] = {startTime[i] , endTime[i] , profit[i]};
+        // }
+
+        //by default -> sorted on the basis of first value but still writing comp for better undertsanding
+        auto comp = [&](auto& vec1 , auto& vec2){
+            return vec1[0] <= vec2[0];
+        };
+
+        //sort it on the basis of start time
+        sort(jobs.begin() , jobs.end() , comp);
+
+        return solve(jobs , startTime ,0);
+    }
+};
+
+
+*/
