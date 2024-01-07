3a45420ad1d6891394efdb820c9b823481ef6271
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-07 11:47:37 +0530

    1

--- Biweekly Contest 110/test.cpp
+++ Biweekly Contest 110/test.cpp
@@ -1,182 +1,94 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int numberOfArithmeticSlices(vector<int> &nums)
+{
 
- int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+    int n = nums.size();
+
+    if (n <= 2)
+        return 0;
+
+    map<long long, int> dp[n + 1];
+
+    long long ans = 0;
+
+    for (int i = 1; i < n; i++)
+    {
         
+        for (int j = 0; j < i; j++)
+        {
+            
+            long long cnt = 1;
+
+            long long diff = (long long)nums[i] - (long long)nums[j];
+
+            if (dp[j].count(diff))
+            {
+                cnt = dp[j][diff];
+                dp[i][diff] += cnt;
+                ans += cnt;
+            }
+
+            dp[i][diff] += 1 ;
+        }
     }
 
+ 
+
+
+    return ans;
+}
 
 int main()
 {
 
-  int n;
-  cin >> n;
+    int n;
+    cin >> n;
 
-  vector<int> arr(n);
-
-  for (int i = 0; i < n; i++)
-  {
-    /* code */
-    cin >> arr[i];
-  }
-
-  vector<int>  start(n);
-  vector<int> profit(n);
+    vector<int> arr(n);
 
     for (int i = 0; i < n; i++)
     {
-      /* code */
-      cin>>start[i];
+        
+        cin >> arr[i];
     }
 
-    for (int i = 0; i < n; i++)
-    {
-      /* code */
-      cin>>profit[i];
-    }
-
-    cout<<jobScheduling(arr,start,profit)<<endl;
-    
-    
-
-  // cout << j(arr);
+   cout<<numberOfArithmeticSlices(arr);
 }
 
 
 
-
-
-
 /*
 
 
 
-
-class Solution {
-
-
-
-public:
-    int jobScheduling(vector<int>& s, vector<int>& e, vector<int>& p) {
-       
-        int n = s.size(); 
-
-
-        vector<pair<int, int>> st(n);
-
-
-        for(int i = 0; i < n; i++) st[i] = {s[i], i};
-
-        sort(st.begin(), st.end());
-
-
-        vector<int> dp(n+1, 0); 
-        
-        for(int i = n-1; i > -1; i--)
-            dp[i] += max(dp[i+1], p[st[i].second] + dp[lower_bound(st.begin() + i, st.end(), make_pair(e[st[i].second], 0)) - st.begin()]);
-
-
-        return dp[0];
-
-
-    }
-
-
-
-};
-
-
-
-*/
-
-
-
-
-
-
-/*
-
-
 class Solution {
 public:
-    int numJobs;
-    int memo[50001];
+    int numberOfArithmeticSlices(std::vector<int>& nums) {
+        int n = nums.size();
+        int totalCount = 0;
 
+        // dp[i][d] represents the number of arithmetic subsequences ending at index i with common difference d
+        std::vector<std::unordered_map<long long, int>> dp(n);
 
-    int getNextIndex(vector<vector<int>>&jobs , int l , int end){
-        int r = numJobs - 1;
-        int res = numJobs;
-        while(l <= r){
-            int mid = l + ((r-l) >> 1);
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                long long diff = (long long)nums[i] - nums[j];
 
-            if(jobs[mid][0] >= end){
-                res = mid;
-                r = mid - 1;
+                // Check for possible integer overflow
+                if (diff < INT_MIN || diff > INT_MAX)
+                    continue;
+
+                int count = dp[j].count(diff) ? dp[j][diff] : 0;
+
+                // Add the count to the total and update dp[i][diff]
+                totalCount += count;
+                dp[i][diff] += count + 1;
             }
-            else{
-                l = mid + 1;
-            }
-        }
-        return res;
-    }
-    int solve(vector<vector<int>>&jobs ,vector<int>&startTime , int i){
-
-        if(i >= numJobs){
-            return 0;
         }
 
-
-        //if we have already calculated the max profit of that job
-        if(memo[i] != -1){
-            return memo[i];
-        }
-
-        //include
-        int next = getNextIndex(jobs , i+1 , jobs[i][1]);
-        //we are using binary search here to get the next index
-        //it helps us to find the lower bound of the next safe job to take
-        //so passing jobs , start , end
-
-        //instead we can also use lower_bound function directly but it is always better to write your own function
-
-        // int next = lower_bound(startTime.begin() , startTime.end() , jobs[i][1]) - startTime.begin();
-
-        int inc = jobs[i][2] + solve(jobs , startTime , next);
-
-
-        //not include
-        int not_inc = solve(jobs , startTime, i + 1);
-
-        return memo[i] = max(inc , not_inc);
-    }
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        numJobs = startTime.size();
-        memset(memo , -1 , sizeof(memo));
-        
-        vector<vector<int>>jobs(numJobs , vector<int>(3,0));
-        for(int i = 0 ; i < numJobs ; i++){
-            jobs[i][0] = startTime[i];
-            jobs[i][1] = endTime[i];
-            jobs[i][2] = profit[i];
-        }
-
-        //we can also create jobs in a diff way
-        // vector<tuple<int,int,int>>jobs(numJobs);
-
-        // for(int i = 0 ; i < numJobs ;i++){
-        //     jobs[i] = {startTime[i] , endTime[i] , profit[i]};
-        // }
-
-        //by default -> sorted on the basis of first value but still writing comp for better undertsanding
-        auto comp = [&](auto& vec1 , auto& vec2){
-            return vec1[0] <= vec2[0];
-        };
-
-        //sort it on the basis of start time
-        sort(jobs.begin() , jobs.end() , comp);
-
-        return solve(jobs , startTime ,0);
+        return totalCount;
     }
 };
 
