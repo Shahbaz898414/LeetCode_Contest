6f9aa5d9ff70ddf69c06259cca935d2797db249f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-07 14:09:36 +0530

    1

--- Biweekly Contest 110/2809. Minimum Time to Make Array Sum At Most x.cpp
+++ Biweekly Contest 110/2809. Minimum Time to Make Array Sum At Most x.cpp
@@ -1,64 +1,144 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+  int n;
+  int dp[1003][1003];
+  int f(vector<vector<int>> &v, int i, int &time, int t)
+  {
+    if (i == n)
+    {
+      return 0;
+    }
+    if (dp[i][t] != -1)
+      return dp[i][t];
+    if (t > time)
+      return 0;
+    int ans = max(f(v, i + 1, time, t), f(v, i + 1, time, t + 1) + v[i][1] + t * v[i][0]);
+    return dp[i][t] = ans;
+  }
 
+  int minimumTime(vector<int> &a, vector<int> &b, int x)
+  {
+    n = a.size();
+    int ans = 0, suma = 0, sumb = 0;
+    vector<vector<int>> v(n);
+    for (int i = 0; i < n; i++)
+    {
+      v[i] = {b[i], a[i]};
+      suma += a[i];
+      sumb += b[i];
+    }
+    sort(v.begin(), v.end());
+    int total = 0;
+    for (int i = 0; i < n; i++)
+    {
+      int xx = n - i - 1;
+      total += (xx * v[i][0]);
+    }
+    if (suma <= x)
+      return 0;
+    if (total > x)
+      return -1;
 
+    int lo = 0, hi = n, mid;
+    while (lo <= hi)
+    {
+      memset(dp, -1, sizeof(dp));
+      mid = (lo + hi) / 2;
+      int c = f(v, 0, mid, 1);
+      int xx = suma + mid * sumb - c;
+      // dbggg(mid, xx, c);
+      if (xx <= x)
+        hi = mid - 1;
+      else
+        lo = mid + 1;
+    }
+    return hi + 1;
+  }
+};
 
-int minimumTime(vector<int>& nums1, vector<int>& nums2, int x) {
-        
-}
+int main()
+{
 
-
-
-int main() {
+  Solution solution;
 
   int n;
   cin >> n;
 
-  vector<int>  arr1(n);
+  vector<int> arr1(n);
 
-  vector<int>  brr(n);
+  vector<int> brr(n);
 
-
-  for (int i = 0; i < n; i++) {
-    cin>>arr1[i];
-  }
-  
-
-  for (int i = 0; i < n; i++) {
-    
-    cin>>brr[i];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr1[i];
   }
 
+  for (int i = 0; i < n; i++)
+  {
 
-  int x;cin>>x;
+    cin >> brr[i];
+  }
 
+  int x;
+  cin >> x;
 
-  cout<<minimumTime(arr1,brr,x);
-
-
-  
+  int result = solution.minimumTime(arr1, brr, x);
+  cout << result;
 }
-
-
-
-
 
 /*
 
-Hi,
 
-I hope this message finds you well! I came across Intern (https://salesforce.wd12.myworkdayjobs.com/en-US/External_Career_Site/job/India---Hyderabad/Intern_JR234461) at Saleforce. As an aspiring Software Engineer, I admire the company's innovative work. If possible, could you kindly refer me? I'd greatly appreciate it!
+class Solution {
+public:
+    int n;
+    int dp[1003][1003];
+    int f(vector<vector<int>> &v, int i, int &time, int t){
+        if(i == n){
+            return 0;
+        }
+        if(dp[i][t] != -1) return dp[i][t];
+        if(t > time) return 0;
+        int ans = max(f(v, i+1, time, t), f(v, i+1, time, t+1) + v[i][1] + t*v[i][0]);
+        return dp[i][t] = ans;
+    }
 
-Best regards,
-Shahbaz Khan
+    int minimumTime(vector<int>& a, vector<int>& b, int x) {
+        n = a.size();
+        int ans = 0, suma = 0, sumb = 0;
+        vector<vector<int>> v(n);
+        for(int i = 0; i < n; i++){
+            v[i] = {b[i], a[i]};
+            suma += a[i];
+            sumb += b[i];
+        }
+        sort(v.begin(), v.end());
+        int total = 0;
+        for(int i = 0; i < n; i++){
+            int xx = n - i - 1;
+            total += (xx * v[i][0]);
+        }
+        if(suma <= x) return 0;
+        if(total > x) return -1;
 
+        int lo = 0, hi = n, mid;
+        while(lo <= hi){
+            memset(dp, -1, sizeof(dp));
+            mid = (lo + hi) / 2;
+            int c = f(v, 0, mid, 1);
+            int xx = suma + mid*sumb - c;
+            // dbggg(mid, xx, c);
+            if(xx <= x) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return hi + 1;
 
+    }
+};
 
-my resume Link:-
-
-https://drive.google.com/file/d/1h9scmsmnC2gtWvpHtVXhgWWsAqfjNjHz/view?usp=sharing
 
 */
-
-
--- Biweekly Contest 110/test.cpp
+++ Biweekly Contest 110/test.cpp
@@ -1,37 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 int numberOfArithmeticSlices(vector<int> &nums)
 {
-
     int n = nums.size();
-
-    if (n <= 2)
-        return 0;
+    if (n <= 2) return 0;
 
     map<long long, int> dp[n + 1];
-
     long long ans = 0;
 
-    for (int i = 1; i < n; i++)
-    {
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++){
         
-        for (int j = 0; j < i; j++)
-        {
-            
             long long cnt = 1;
 
-            long long diff = (long long)nums[i] - (long long)nums[j];
+            long long diff=(long long)nums[i] - (long long)nums[j];
 
-            if (dp[j].count(diff))
-            {
+            if (dp[j].count(diff)) {
                 cnt = dp[j][diff];
                 dp[i][diff] += cnt;
                 ans += cnt;
             }
 
-            dp[i][diff] += 1 ;
+
+            dp[i][diff] += 1;
+
+            cout<<i+1<<" "<<j+1<<" "<<diff<<endl;
         }
+
+
+        cout<<endl;
+
+
     }
 
  
@@ -39,6 +41,8 @@ int numberOfArithmeticSlices(vector<int> &nums)
 
     return ans;
 }
+
+
 
 int main()
 {
@@ -59,38 +63,3 @@ int main()
 
 
 
-/*
-
-
-
-class Solution {
-public:
-    int numberOfArithmeticSlices(std::vector<int>& nums) {
-        int n = nums.size();
-        int totalCount = 0;
-
-        // dp[i][d] represents the number of arithmetic subsequences ending at index i with common difference d
-        std::vector<std::unordered_map<long long, int>> dp(n);
-
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                long long diff = (long long)nums[i] - nums[j];
-
-                // Check for possible integer overflow
-                if (diff < INT_MIN || diff > INT_MAX)
-                    continue;
-
-                int count = dp[j].count(diff) ? dp[j][diff] : 0;
-
-                // Add the count to the total and update dp[i][diff]
-                totalCount += count;
-                dp[i][diff] += count + 1;
-            }
-        }
-
-        return totalCount;
-    }
-};
-
-
-*/
