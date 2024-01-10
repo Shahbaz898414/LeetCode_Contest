ed990708e5efc8eb04593c0e56969b249b4dfe0e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-10 23:34:37 +0530

    1

--- Biweekly Contest 121/2997. Minimum Number of Operations to Make Array XOR Equal to K.cpp
+++ Biweekly Contest 121/2997. Minimum Number of Operations to Make Array XOR Equal to K.cpp
@@ -1,55 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int minOperations(vector<int> &nums, int k)
+{
+  vector<int> oneCount(32, 0);
+  for (int num : nums)
+  {
+    for (int i = 0; i < 32; i++)
+    {
+      if (num & (1 << i))
+        oneCount[i]++;
+    }
+  }
 
+  int ans = 0;
+  for (int i = 0; i < 32; i++)
+  {
+    if (k & (1 << i))
+    {
+      if (oneCount[i] % 2 == 0)
+        ans++;
+    }
+    else
+    {
+      if (oneCount[i] % 2 != 0)
+        ans++;
+    }
+  }
 
-int minOperations(vector<int>& nums, int k) {
-  vector<int>oneCount(32, 0);
-        for (int num : nums)
-        {
-            for (int i = 0; i < 32; i++)
-            {
-                if (num & (1 << i))
-                    oneCount[i]++;
-            }
-        }
-        
-
-        int ans = 0;
-        for (int i = 0; i < 32; i++)
-        {
-            if (k & (1 << i)) 
-            {
-                if (oneCount[i] % 2 == 0) 
-                    ans++;
-            }
-            else   
-            {
-                if (oneCount[i] % 2 != 0) 
-                    ans++;
-            }
-        }
-
-        return ans;
+  return ans;
 }
+
+
 
 
 int main()
 {
 
-    int n;
-    cin >> n;
+  int n;
+  cin >> n;
 
   vector<int> arr(n);
 
   for (int i = 0; i < n; i++)
   {
     /* code */
-    cin>>arr[i];
+    cin >> arr[i];
   }
-  
 
-  int k;cin>>k;
-    
+  int k;
+  cin >> k;
 }
-
