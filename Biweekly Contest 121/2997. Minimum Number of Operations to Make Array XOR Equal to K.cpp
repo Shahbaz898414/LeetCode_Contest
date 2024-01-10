326dea352fb84ce55d048b8c6bf0abb172df5a6e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-10 23:31:03 +0530

    1

--- Biweekly Contest 121/2997. Minimum Number of Operations to Make Array XOR Equal to K.cpp
+++ Biweekly Contest 121/2997. Minimum Number of Operations to Make Array XOR Equal to K.cpp
@@ -14,17 +14,18 @@ int minOperations(vector<int>& nums, int k) {
             }
         }
         
+
         int ans = 0;
         for (int i = 0; i < 32; i++)
         {
-            if (k & (1 << i)) //if ith bit in k = 1 (set)
+            if (k & (1 << i)) 
             {
-                if (oneCount[i] % 2 == 0) //we wanted odd number of 1's but got even number of 1's
+                if (oneCount[i] % 2 == 0) 
                     ans++;
             }
-            else   //if ith bit in k = 0 (unset)
+            else   
             {
-                if (oneCount[i] % 2 != 0) //we wanted even number of 1's but got odd number of 1's
+                if (oneCount[i] % 2 != 0) 
                     ans++;
             }
         }
@@ -52,59 +53,3 @@ int main()
     
 }
 
-
-
-
-/*
-
-
-int minOperations(vector<int>& A, int k) {
-        for (int a : A)
-            k ^= a;
-        return __builtin_popcount(k);
-}
-
-
-*/
-
-
-
-/*
-
-
-class Solution {
-public:
-    int minOperations(vector<int>& nums, int k) 
-    {
-        vector<int>oneCount(32, 0);
-        for (int num : nums)
-        {
-            for (int i = 0; i < 32; i++)
-            {
-                if (num & (1 << i))
-                    oneCount[i]++;
-            }
-        }
-        
-        int ans = 0;
-        for (int i = 0; i < 32; i++)
-        {
-            if (k & (1 << i)) //if ith bit in k = 1 (set)
-            {
-                if (oneCount[i] % 2 == 0) //we wanted odd number of 1's but got even number of 1's
-                    ans++;
-            }
-            else   //if ith bit in k = 0 (unset)
-            {
-                if (oneCount[i] % 2 != 0) //we wanted even number of 1's but got odd number of 1's
-                    ans++;
-            }
-        }
-
-        return ans;
-        
-    }
-};
-
-
-*/
