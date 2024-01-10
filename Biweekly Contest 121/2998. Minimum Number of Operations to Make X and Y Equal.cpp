11c41443b7f6b8d3f4e26da462944933995c16e3
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-11 00:33:01 +0530

    1

--- Biweekly Contest 121/2998. Minimum Number of Operations to Make X and Y Equal.cpp
+++ Biweekly Contest 121/2998. Minimum Number of Operations to Make X and Y Equal.cpp
@@ -2,19 +2,42 @@
 using namespace std;
 
 
+  int solve(int x, int &y, vector<int> &dp) {
+        if (x < y) return (y - x);
+
+        if (x == y) return 0;
+
+        if (dp[x] != -1) return dp[x];
+
+        int opt1 = 1e9;
+        if ((x % 11) == 0) {
+            opt1 = 1 + solve(x / 11, y, dp);
+        } else {
+            int opt1_1 = 1 + (x % 11) + solve(x / 11, y, dp);
+            int opt1_2 = 1 + (11 - (x % 11)) + solve((x / 11) + 1, y, dp);
+            opt1 = min(opt1_1, opt1_2);
+        }
+
+        int opt2 = 1e9;
+
+        if ((x % 5) == 0) {
+            opt2 = 1 + solve(x / 5, y, dp);
+        }else {
+            int opt2_1 = 1 + (x % 5) + solve(x / 5, y, dp);
+            int opt2_2 = (5 - (x % 5)) + solve((x / 5) + 1, y, dp) + 1;
+            opt2 = min(opt2_1, opt2_2);
+        }
+
+        int opt3 = 1e9;
+        opt3 = 1 + solve(x - 1, y, dp);
+
+        return dp[x] = min({opt1, opt2, opt3});
+  }
+
 int minimumOperationsToMakeEqual(int x, int y) {
-        
-    if(y>x){
-      return y-x;
+        vector<int> dp(1e4 + 1, -1);
+        return solve(x, y, dp);
     }
-
-    if(x==y)  return 0;
-
-    
-
-
-        
-}
 
 
 int main()
@@ -29,29 +52,3 @@ int main()
 
 
 
-/*
-
-
-vector<int> dp;
-
-
-int solve(int x, int y){
-    if(x <= y) return y - x;
-    if(dp[x] != -1) return dp[x];
-    int res = abs(x - y);                                 //case 1
-    res = min(res, 1 + x%5 + solve(x/5, y));              //case 2
-    res = min(res, 1 + (5 - x%5) + solve(x/5 + 1, y));    //case 3
-    res = min(res, 1 + x%11 + solve(x/11, y));            //case 4
-    res = min(res, 1 + (11 - x%11) + solve(x/11 + 1, y)); //case 5
-    return dp[x] = res;
-}
-
-
-int minimumOperationsToMakeEqual(int x, int y) {
-    dp.resize(10011, -1);
-    return solve(x, y);
-}
-
-
-
-*/
