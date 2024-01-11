a521e9112e1e0b772ad33a2687147215a6fc0432
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-11 10:02:19 +0530

    1

---
+++ Biweekly Contest 121/Count the Number of Powerful Integers.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
+
+
+
+
+}
+
+ 
+
+int main() {
+
+
+  long long x,y,z;cin>>x>>y>>z;
+
+
+  string s;cin>>s;
+
+
+  cout<<numberOfPowerfulInt(x,y,z,s)<<endl;
+
+
+
+}
+
+
+
+
+/*
+
+*/
