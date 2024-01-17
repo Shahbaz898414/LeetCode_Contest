4f6ebb24dcb2a95592fbf6ab1ab53467427b4643
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-17 13:18:46 +0530

    1

--- Weekly Contest 380/Count Elements With Maximum Frequency.cpp
+++ Weekly Contest 380/Count Elements With Maximum Frequency.cpp
@@ -2,7 +2,28 @@
 using namespace std;
 
 
+int maxFrequencyElements(vector<int>& nums) {
+   map<int,int> mp;
 
+   for(int i=0;i<nums.size();i++){
+    mp[nums[i]]++;
+   }     
+
+   int mx=0;
+
+   for(auto it:mp){
+    mx=max(mx,it.second);
+   }
+
+  int cnt=0;
+  for(auto it:mp){
+    if(it.second==mx){
+      cnt+=it.second;
+    }
+  }
+
+  return cnt;
+}
 
 int main()
 {
@@ -17,6 +38,8 @@ int main()
 
         cin >> arr[i];
     }
+
+    cout<<maxFrequencyElements(arr)<<endl;
 
     
 }
---
+++ Weekly Contest 380/Find Beautiful Indices in the Given Array I.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> beautifulIndices(string s, string a, string b, int k) {
+        
+    }
+
+int main()
+{
+
+    string s,a,b;
+    cin>>s>>a>>b;
+
+    int k;cin>>k;
+
+
+
+  vector<int> v=beautifulIndices(s,a,b,k);
+   
+  
+    
+}
+
+
