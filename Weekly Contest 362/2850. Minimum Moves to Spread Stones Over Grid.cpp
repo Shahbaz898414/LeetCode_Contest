782f1b75ac466f4edc90b22a1033168a52174424
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-02 01:31:38 +0530

    1

---
+++ Weekly Contest 362/2850. Minimum Moves to Spread Stones Over Grid.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minimumMoves(vector<vector<int>> &grid)
+{
+}
+
+int main()
+{
+
+  int n=9;
+  // cin >> n;
+
+  vector<vector<int>> arr(9, vector<int>(9));
+
+  for (int i = 0; i < n; i++)
+  {
+    /* code */
+    for (int j = 0; j < n; j++)
+    {
+      /* code */
+      cin >> arr[i][j];
+    }
+  }
+}
+
+
+/*
+[Bug]: Spelling error
+
+*/
